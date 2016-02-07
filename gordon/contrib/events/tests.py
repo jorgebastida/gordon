d32b9b019f4629fc67ffdf8a22331ac8737388f9
Jorge Bastida <me@jorgebastida.com>
2016-02-07 21:30:19 +0000

    Fix tests

--- gordon/contrib/events/tests.py
+++ gordon/contrib/events/tests.py
@@ -147,61 +147,76 @@ class TestContribEventsTarget(unittest.TestCase):
         send_mock.assert_called_once_with(
             event, context, SUCCESS, physical_resource_id='rule-targets-rule_name'
         )
-    #
-    # @patch('gordon.contrib.events.target.target.send')
-    # @patch('gordon.contrib.events.target.target.boto3.client')
-    # def test_rule_update(self, boto3_client, send_mock):
-    #     client = Mock()
-    #     boto3_client.return_value = client
-    #     context = MockContext()
-    #     event = {
-    #         'RequestType': 'Update',
-    #         'ResourceProperties': {
-    #             'Name': 'rule_name',
-    #             'ScheduleExpression': 'cron(0 20 * * ? *)',
-    #             'State': 'ENABLED',
-    #             'Description': 'My description',
-    #         }
-    #     }
-    #     client.get_rule.return_value = True
-    #     client.put_rule.return_value = {'RuleArn': 'rule_arn'}
-    #
-    #     rule.handler(event, context)
-    #
-    #     client.put_rule.assert_called_once_with(
-    #         Name='rule_name',
-    #         State='ENABLED',
-    #         ScheduleExpression='cron(0 20 * * ? *)',
-    #         EventPattern='',
-    #         Description='My description',
-    #         RoleArn=''
-    #     )
-    #
-    #     send_mock.assert_called_once_with(
-    #         event, context, SUCCESS, response_data={'Arn': 'rule_arn'}
-    #     )
-    #
-    # @patch('gordon.contrib.events.target.target.send')
-    # @patch('gordon.contrib.events.target.target.boto3.client')
-    # def test_rule_delete(self, boto3_client, send_mock):
-    #     client = Mock()
-    #     boto3_client.return_value = client
-    #     context = MockContext()
-    #     event = {
-    #         'RequestType': 'Delete',
-    #         'ResourceProperties': {
-    #             'Name': 'rule_name',
-    #             'ScheduleExpression': 'cron(0 20 * * ? *)',
-    #             'State': 'ENABLED',
-    #             'Description': 'My description',
-    #         }
-    #     }
-    #     client.get_rule.return_value = True
-    #     client.put_rule.return_value = {'RuleArn': 'rule_arn'}
-    #
-    #     rule.handler(event, context)
-    #
-    #     client.put_rule.assert_not_called()
-    #     send_mock.assert_called_once_with(
-    #         event, context, SUCCESS
-    #     )
+    @patch('gordon.contrib.events.target.target.send')
+    @patch('gordon.contrib.events.target.target.boto3.client')
+    def test_target_update(self, boto3_client, send_mock):
+        client = Mock()
+        boto3_client.return_value = client
+        context = MockContext()
+        event = {
+            'RequestType': 'Update',
+            'ResourceProperties': {
+                'Rule': 'rule_name',
+                'Targets': [
+                    {
+                        'Id': 'target_0_id',
+                        'Arn': 'target_0_arn',
+                        'Input': 'target_0_input',
+                        'InputPath': 'target_0_input_path',
+                    }
+                ]
+            }
+        }
+
+        client.put_targets.return_value = {}
+        target.handler(event, context)
+
+        client.put_targets.assert_called_once_with(
+            Rule='rule_name',
+            Targets=[
+                {
+                    'Id': 'target_0_id',
+                    'Arn': 'target_0_arn',
+                    'Input': 'target_0_input',
+                    'InputPath': 'target_0_input_path',
+                }
+            ]
+        )
+
+        send_mock.assert_called_once_with(
+            event, context, SUCCESS, physical_resource_id='rule-targets-rule_name'
+        )
+
+    @patch('gordon.contrib.events.target.target.send')
+    @patch('gordon.contrib.events.target.target.boto3.client')
+    def test_target_delete(self, boto3_client, send_mock):
+        client = Mock()
+        boto3_client.return_value = client
+        context = MockContext()
+        event = {
+            'RequestType': 'Delete',
+            'ResourceProperties': {
+                'Rule': 'rule_name',
+                'Targets': [
+                    {
+                        'Id': 'target_0_id',
+                        'Arn': 'target_0_arn',
+                        'Input': 'target_0_input',
+                        'InputPath': 'target_0_input_path',
+                    }
+                ]
+            }
+        }
+        client.list_targets_by_rule.return_value = {
+            'Targets': [
+                {'Id': '1'},
+                {'Id': '2'}
+            ]
+        }
+        target.handler(event, context)
+        client.put_targets.assert_not_called()
+        client.remove_targets.assert_called_once_with(
+            Rule='rule_name',
+            Ids=['1', '2']
+        )
+        send_mock.assert_called_once_with(event, context, SUCCESS)
